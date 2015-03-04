6ae9a9dbb1670e8581d55369d1479271722c79c9
sepisoad <sepehr.aryani@gmail.com>
2015-03-04 20:34:25 +0330

    =oh man

--- nakefile.nim
+++ nakefile.nim
@@ -5,9 +5,9 @@ const EXE_NAME = "  --out:./condooj ./src/condooj.nim "
 const COMMON_FLAGS = " -d:RUN_AS_APP "
 const DEBUG_FLAGS = " c --debuginfo --lineDir:on -d:useSysAssert -d:useGcAssert --nimcache:./BUILD/DEBUG/"
 const RELEASE_FLAG = " -d:release "
+const THROW_NAKE_CATCH = " --nimcache:./BUILD/NAKECATCH "
 
 task "debug", "this option will compile source codes under debug mode":
-  echo(nimExe & DEBUG_FLAGS & COMMON_FLAGS & EXE_NAME)
   if not shell(nimExe & DEBUG_FLAGS & COMMON_FLAGS & EXE_NAME):
     echo("Error")
   else:
